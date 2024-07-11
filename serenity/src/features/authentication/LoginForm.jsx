70c47aca08edc336482e9b642753d727832eee8c
Utkarsh Shrivastava <utkarshshrivastavajee@gmail.com>
2024-07-11 22:11:25 +0530

    sync

--- serenity/src/features/authentication/LoginForm.jsx
+++ serenity/src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('utkarsh@example.com');
+  const [password, setPassword] = useState('Manas@20');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
